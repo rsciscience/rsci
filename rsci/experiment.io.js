a7f36a5d31468629b366cacd9b51a4508e8825e0
David McColgan <david.mccolgan@googelmail.com>
2018-06-03 20:58:33 -0700

    test

--- rsci/experiment.io.js
+++ rsci/experiment.io.js
@@ -1,29 +1,50 @@
 "use strict";
 const debug = require('debug')('RSCI.experiment.io');
-const Gpio = require('onoff').Gpio;
 var helpers = require('../helpers');
 
-var pin_food = new Gpio(1, 'out');
-var pin_drug = new Gpio(2, 'out');
-
-this.dispenseFood = function () {
-  debug('dispenseFood');
-  try {
-    pin_food.writeSync(1);
-    setTimeout(1000, () => { pin_food.writeSync(1); })
-  } catch (e) {
-    console.log(e);
-  }
-};
-this.dispenseDrug = function () {
-  debug('dispenseDrug');
-  try {
-    pin_drug.writeSync(1);
-    setTimeout(1000, ()=>{  pin_drug.writeSync(1); })
-  }catch (e){
-    console.log(e);
-  }
+try {
   
-};
+  const Gpio = require('onoff').Gpio;
+  var pin_food = new Gpio(1, 'out');
+  var pin_drug = new Gpio(2, 'out');
 
-module.exports = this;
+  this.dispenseFood = function () {
+    debug('dispenseFood');
+    try {
+      pin_food.writeSync(1);
+      setTimeout(1000, () => { pin_food.writeSync(1); })
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  this.dispenseDrug = function () {
+    debug('dispenseDrug');
+    try {
+      pin_drug.writeSync(1);
+      setTimeout(1000, () => { pin_drug.writeSync(1); })
+    } catch (e) {
+      console.log(e);
+    }
+
+  };
+
+
+  module.exports = this;
+
+} catch (e) {
+
+    // test stub
+  this.dispenseFood = function () {
+    debug('dispenseFood');
+    console.log('IO not configured. Using test stubs');
+  };
+  this.dispenseDrug = function () {
+    debug('dispenseDrug');
+    console.log('IO not configured. Using test stubs');
+  };
+  module.exports = this;
+
+}
+
+
+
