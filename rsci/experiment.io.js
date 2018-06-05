f5b4e8433cc0c4a85cb3a659b497550a7f7c899a
David McColgan <david.mccolgan@googelmail.com>
2018-06-04 19:15:36 -0700

    test

--- rsci/experiment.io.js
+++ rsci/experiment.io.js
@@ -7,31 +7,45 @@ try {
   const Gpio = require('onoff').Gpio;
   var pin_food = new Gpio(2, 'out');
   var pin_drug = new Gpio(3, 'out');
+  var pin_buzzer = new Gpio(4, 'out');
 
   this.restIO = function (){
     pin_drug.writeSync(0);
-    pin_drug.writeSync(0);
+    pin_food.writeSync(0);
+    pin_buzzer.writeSync(0);
   }
-
 
   this.dispenseFood = function () {
     debug('dispenseFood');
     try {
       pin_food.writeSync(1);
-      setTimeout(1000, () => { pin_food.writeSync(1); })
+      setTimeout(1000, () => { pin_food.writeSync(0); })
     } catch (e) {
+      console.log('IO Error ');
       console.log(e);
     }
   };
+
   this.dispenseDrug = function () {
     debug('dispenseDrug');
     try {
       pin_drug.writeSync(1);
       setTimeout(1000, () => { pin_drug.writeSync(0); })
     } catch (e) {
+      console.log('IO Error ');
       console.log(e);
     }
+  };
 
+  this.dispenseDrug = function () {
+    debug('dispenseDrug');
+    try {
+      pin_buzzer.writeSync(1);
+      setTimeout(1000, () => { pin_buzzer.writeSync(0); })
+    } catch (e) {
+      console.log('IO Error ');
+      console.log(e);
+    }
   };
 
   this.resetIO();
