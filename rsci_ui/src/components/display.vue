24ef777650278707c57f075188b63a76c7aab7b2
fergmac <f.macconnell@gmail.com>
2018-07-03 21:28:59 -0700

    Make eslint changes

--- rsci_ui/src/components/display.vue
+++ rsci_ui/src/components/display.vue
@@ -59,8 +59,8 @@ export default {
       }
       console.log('client_experment_stop', val)
     },
-    heartbeat_check: function() {
-       this.event("heartbeat_response")
+    heartbeat_check: function () {
+      this.event('heartbeat_response')
     }
   },
   data () {
