f27527296b4f2aaab62a6665e13273d01fd93a39
ahewgill <adam@hewgillsystems.ca>
2018-10-23 20:43:06 -0700

    Making index.js less this-y

--- index.js
+++ index.js
@@ -1,78 +1,73 @@
-"use strict";
-var ip = require('ip');
-const debug = require('debug')('RSCI.index');
-const request = require('request-promise');
-const server = require('./rsci/server');
-const client = require('./rsci/client');
+"use strict"
+const ip = require('ip')
+const debug = require('debug')('RSCI.index')
+const server = require('./rsci/server')
+const client = require('./rsci/client')
 
 process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
   // application specific logging, throwing an error, or other logic here
-});
+})
 
-debug('Init:RSCI');
+debug('Init:RSCI')
 
-console.log('');
-console.log('  )    ');
-console.log(' (__   ');
-console.log(' _  )_ ');
-console.log('(_)_(_)');
-console.log(' (o o) ');
-console.log('==\\o/==');
-console.log('');
+console.log('')
+console.log('  )    ')
+console.log(' (__   ')
+console.log(' _  )_ ')
+console.log('(_)_(_)')
+console.log(' (o o) ')
+console.log('==\\o/==')
+console.log('')
 
-var api = require('./rsci/api');
-debug('Init Api Server');
+const api = require('./rsci/api')
+debug('Init Api Server')
 
-this.client = new client();
-this.server = new server();
-this.db = require('./rsci/db');
-this.export = require('./rsci/export');
-this.state = require('./rsci/state');
-this.helpers = require('./rsci/helpers')
+const db = require('./rsci/db')
+const exp = require('./rsci/export')
+const state = require('./rsci/state')
 
 
-this.initSettings = async function () {
-  debug('initSettings');
+async function initSettings() {
+  debug('initSettings')
 
-  var data = await this.db.settings.read();
+  var data = await db.settings.read()
   debug('Read settings')
   
   if (data && data.clientId) {
-    this.state.clientId = data.clientId;
+    state.clientId = data.clientId
   } else {
-    this.state.clientId = 'id_' + ip.address();
-    await this.db.settings.save({ clientId: this.state.clientId });
-    debug('Saved settings');
+    state.clientId = 'id_' + ip.address()
+    await db.settings.save({ clientId: state.clientId })
+    debug('Saved settings')
   }
   if (data && data.isServer) {
-    this.state.isServer = data.isServer;
+    state.isServer = data.isServer
   }
   
-  this.state.me = {
+  state.me = {
     me: true,
     ip: ip.address(),
-    port: this.state.listeningPort,
-    clientId: this.state.clientId,
-    initTimeStamp: this.state.initTimeStamp,
-  };
-}.bind(this);
-
-
-this.init = async function () {
-  debug('init')
-  await this.initSettings()
-
-  this.state.experiments.configs = this.server.experiments.load(this.state.experiments.configDir);
-  api.init(this.state.listeningPort, this.client, this.server, this.export);
-
-  if (this.state.isServer === true) {
-    debug('I\'m the server')
-    this.server.register()
-  } else {
-    this.client.search()
+    port: state.listeningPort,
+    clientId: state.clientId,
+    initTimeStamp: state.initTimeStamp,
   }
-}.bind(this)
+}
 
+async function init() {
+  debug('init')
+  await initSettings()
 
-this.init()
+  const cl = new client()
+  const srv = new server()
+  state.experiments.configs = srv.experiments.load(state.experiments.configDir)
+  api.init(state.listeningPort, cl, srv, exp)
+
+  if (state.isServer === true) {
+    srv.register()
+  } else {
+    cl.search()
+  }
+}
+
+init()
